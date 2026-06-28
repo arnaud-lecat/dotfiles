7d156a3de4398f0624bef3fc95f21bf4f1d28af8
Arnaud Lecat <bonjour@arnaudlecat.com>
2026-06-28 21:41:08 +0200

    Update

--- home/dot_config/private_fish/config.fish
+++ home/dot_config/private_fish/config.fish
@@ -1,11 +1,15 @@
-fastfetch
-
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    fastfetch
 end
+
 set -gx EDITOR nvim
+
 set -gx CUDA_HOME /opt/cuda
+set -gx PYTORCH_CUDA_ALLOC_CONF expandable_segments:True
+set -gx LD_LIBRARY_PATH $CUDA_HOME/lib64 $LD_LIBRARY_PATH
+
 set -gx LV_BRANCH release-1.4/neovim-0.9
+
 set -gx LD_LIBRARY_PATH $CUDA_HOME/lib64 $LD_LIBRARY_PATH
 set -gx PIP_REQUIRE_VIRTUALENV true
 set -gx PATH ~/.npm-global/bin $PATH
@@ -15,12 +19,12 @@ alias vi=nvim
 # pnpm
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
 
 if not contains "$HOME/.local/bin" $PATH
-	set -gx PATH "$HOME/.local/bin" $PATH
+    set -gx PATH "$HOME/.local/bin" $PATH
 end
 
 starship init fish | source
